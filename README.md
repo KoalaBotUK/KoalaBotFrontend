04f182da2c2adee4e2b01a999c596cd2b878901c
Stefan Cooper <stefan.cooper27@gmail.com>
2020-12-31 15:45:21 +0000

    Add codebase structure for Pages (Templates and Stylesheets) (#1)
    * Add frontend page structures
    
    * Add frontend page structures
    
    * Add new lines at end of files
    
    * Updated requirements

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # KoalaBotFrontend
 KoalaBot Frontend web application
+
+### Prerequisites
+
+The preferred way to run this webapp locally is by using [virtualenvwrapper](https://pypi.org/project/virtualenvwrapper/) to create a virtualenv for python3.
+
+To do this:
+
+1. Install [python3](https://docs.python.org/3/using/index.html)
+2. Install [virtualenvwrapper](https://virtualenvwrapper.readthedocs.io/en/latest/install.html)
+
+
+### Installing
+
+
+
+Make a new virtual environment for the project
+
+```
+mkvirtualenv venv
+```
+
+Work on the virtual environment
+```
+workon venv
+```
+install the python3 dependencies
+```
+pip3 install -r requirements.txt
+```
+
+## Running the server
+To run the server:
+```
+./runserver
+```
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.template import loader
+from django import template
+
 # Create your views here.
+
+
+def homepage(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render({}, request))
+
+def settings(request):
+    template = loader.get_template('settings.html')
+    return HttpResponse(template.render({}, request))
+
+def twitch(request):
+    template = loader.get_template('twitch.html')
+    return HttpResponse(template.render({}, request))
+
+def verify(request):
+    template = loader.get_template('verify.html')
+    return HttpResponse(template.render({}, request))
+    
