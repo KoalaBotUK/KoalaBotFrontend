983c3ebb7b676bb08b1a0d429fa8b58ea1ab8c74
Stefan Cooper <stefan.cooper27@gmail.com>
2021-01-06 22:48:26 +0000

    Add link to KoalaBot backend (#8)
    * Add koala backend link
    
    * PR Changes

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,33 @@
 from django.http import HttpResponse
 from django.template import loader
 
+# This is only an example to document how you might, we can delete this once we have some cog stuff in
+def exampleBackend():
+    # Import KoalaBot like this
+    import KoalaBot
+    from KoalaBot import database_manager
+
+    # Set up bot information like this
+    database_manager = KoalaBot.database_manager
+    bot = KoalaBot.commands.Bot(command_prefix=KoalaBot.COMMAND_PREFIX)
+
+    # Import a cog like this
+    # from cogs import TextFilter
+
+    # Import a cog Database manager like this
+    from cogs.TextFilter import TextFilterDBManager
+    tf_database_manager = TextFilterDBManager(database_manager, bot)
+    tf_database_manager.create_tables()
+
+    try:
+        tf_database_manager.new_filtered_text(1, "test", "banned", False)
+    except Warning:
+        print("Already added")
+
+    print(tf_database_manager.get_filtered_text_for_guild(1))
+
+# exampleBackend()
+
 # Create your views here.
 
 def homepage(request):
