a433fff40a6492f11ff90d806866469c0d7611f0
Joe <JCoder00@users.noreply.github.com>
2021-02-13 15:29:02 +0000

    Added functions for Frontend BaseCog extensions

--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 
+import KoalaBot
+
+"""
 # This is only an example to document how you might, we can delete this once we have some cog stuff in
 def exampleBackend():
     # Import KoalaBot like this
-    import KoalaBot
     from KoalaBot import database_manager
 
     # Set up bot information like this
@@ -26,7 +28,62 @@ def exampleBackend():
 
     print(tf_database_manager.get_filtered_text_for_guild(1))
 
-# exampleBackend()
+#exampleBackend()
+"""
+
+class BaseCogFront:
+
+    def __init__(self):
+        bot = KoalaBot.commands.Bot(command_prefix=KoalaBot.COMMAND_PREFIX)
+
+        #from cogs import BaseCog
+    '''
+    def load_cog_frontend (self, extension):
+        """
+        Loads a cog from the cogs folder
+        :param extension: The name of the cog
+        """
+        try:
+            self.bot.load_extension(self.COGS_DIR.replace("/", ".")+f'.{extension}')
+            print(f"{extension} loaded successfully.")
+        except Warning:
+            print(f"Cog '{extension}' already loaded.")
+
+    def unload_cog_frontend (self, extension):
+        """
+        Unloads a running cog
+        :param extension: The name of the cog
+        """
+        try:
+            if extension == "BaseCog":
+                # Error message here to user
+                print("Sorry, you can't unload the base cog")
+            else:
+                self.bot.unload_extension(self.COGS_DIR.replace("/", ".") + f'.{extension}')
+                print(f"{extension} unloaded successfully.")
+        except Warning:
+            print(f"Error unloading cog {extension}.")
+    '''
+
+    def enable_koala_extesion(self, guild_id, koala_extension):
+        if koala_extension.lower() in ["all"]:
+            available_extensions = KoalaBot.database_manager.get_all_available_guild_extensions(guild_id)
+            for extension in available_extensions:
+                KoalaBot.database_manager.give_guild_extension(guild_id, extension[0])
+        else:
+            KoalaBot.database_manager.give_guild_extension(guild_id, koala_extension)
+
+    def disable_koala_extension(self, guild_id, koala_extension):
+        all_ext = KoalaBot.database_manager.get_enabled_guild_extensions(guild_id)
+        if koala_extension.lower() in ["all"]:
+            for ext in all_ext:
+                KoalaBot.database_manager.remove_guild_extension(guild_id, ext[0])
+        elif (koala_extension,) not in all_ext:
+            raise NotImplementedError(f"{koala_extension} is not an enabled extension")
+        KoalaBot.database_manager.remove_guild_extension(guild_id, koala_extension)
+
+    def list_koala_extensions(self, guild_id):
+        return KoalaBot.database_manager.get_all_available_guild_extensions(guild_id)
 
 # Create your views here.
 
@@ -49,5 +106,3 @@ def verify(request):
 def socialAlert(request):
     loadTemplate = loader.get_template('social_alert.html')
     return HttpResponse(loadTemplate.render({}, request))
-
-    
